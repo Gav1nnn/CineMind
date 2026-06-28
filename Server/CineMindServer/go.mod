9e90a6bf2c03b9af53ee1c4574e55f0a2b9da3dc
Gavin Zhang <gav1nnnzzz0@gmail.com>
2026-06-28 23:39:33 +0800

    add token utils func

--- Server/CineMindServer/go.mod
+++ Server/CineMindServer/go.mod
@@ -5,8 +5,10 @@ go 1.26.4
 require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/go-playground/validator/v10 v10.30.3
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver/v2 v2.7.0
+	golang.org/x/crypto v0.53.0
 )
 
 require (
@@ -37,7 +39,6 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.28.0 // indirect
-	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/net v0.56.0 // indirect
 	golang.org/x/sync v0.21.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
---
+++ Server/CineMindServer/utils/token_util.go
@@ -0,0 +1,85 @@
+package utils
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/Gav1nnn/CineMind/Server/CineMindServer/database"
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Role      string
+	UserID    string
+	jwt.RegisteredClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+var SECRET_REFRESH_KEY string = os.Getenv("SECRET_REFRESH_KEY")
+var userCollection *mongo.Collection = database.OpenCollection("users")
+
+func GenerateAllTokens(email, firstName, lastName, role, userid string) (string, string, error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		UserID:    userid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "CineMind",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	signedToken, err := token.SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshClaims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		UserID:    userid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "CineMind",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims)
+	signedRefreshToken, err := refreshToken.SignedString([]byte(SECRET_REFRESH_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	return signedToken, signedRefreshToken, nil
+}
+
+func UpdateAllTokens(userId, token, refreshtoken string) (err error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	updateAt, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateData := bson.M{
+		"$set": bson.M{
+			"token":         token,
+			"refresh_token": refreshtoken,
+			"update_at":     updateAt,
+		},
+	}
+	_, err = userCollection.UpdateOne(ctx, bson.M{"user_id": userId}, updateData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
